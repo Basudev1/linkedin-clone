83bb12af03d2e1980df71683ca1c3d571a8338a5
Basudev <murabasudev@gmail.com>
2022-01-22 14:03:14 +0530

    Added Recoil, modal

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
+import { AnimatePresence } from "framer-motion";
 import Head from "next/head";
 import Image from "next/image";
 import { signOut, getSession, useSession } from "next-auth/react";
 import Header from "../components/Header";
+import { useRecoilState } from "recoil";
+import { modalState, modalTypeState } from "../atoms/modalAtom";
 import Sidebar from "../components/Sidebar";
 import { useRouter } from "next/router";
+import Feed from "../components/Feed";
+import Modal from "../components/Modal";
 export default function Home() {
+  const [modalOpen, setModalOpen] = useRecoilState(modalState);
+  const [modalType, setModalType] = useRecoilState(modalTypeState);
   const router = useRouter();
   const { status } = useSession({
     required: true,
@@ -28,9 +35,14 @@ export default function Home() {
       <main className="flex justify-center gap-x-5 px-4 sm:px-12">
         <div className="flex flex-col md:flex-row gap-5">
           <Sidebar />
-          {/* Feed */}
+          <Feed />
         </div>
         {/* Widgets */}
+        <AnimatePresence>
+          {modalOpen && (
+            <Modal handleClose={() => setModalOpen(false)} type={modalType} />
+          )}
+        </AnimatePresence>
       </main>
     </div>
   );
